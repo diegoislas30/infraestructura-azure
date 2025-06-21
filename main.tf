67fb59c393aa2c24c33cc2fcf5fa9aa68ee7ef89
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-06-20 21:45:00 -0600

    se añade virtual network

--- main.tf
+++ main.tf
@@ -4,3 +4,24 @@ resource "azurerm_resource_group" "rg-terraform" {
   name     = "terraform-poc"
   location = var.location
 }
+
+
+resource "azurerm_virtual_network" "vnet-terraform" {
+  name                = "vnet-terraform"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg-terraform.location
+  resource_group_name = azurerm_resource_group.rg-terraform.name
+  tags = {
+    environment = "Terraform"
+  }
+}
+
+resource "azurerm_subnet" "subnet-terraform" {
+  name                 = "subnet-terraform"
+  resource_group_name  = azurerm_resource_group.rg-terraform.name
+  virtual_network_name = azurerm_virtual_network.vnet-terraform.name
+  address_prefixes     = ["10.0.1.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  
+
+}
