9f312c7e528e95283bc056e93e2272f08e88877e
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-06-22 21:30:58 -0600

    se corrige ubicación del rg ptm

--- modules/resources_group/main.tf
+++ modules/resources_group/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_resource_group" "rg2" {
   name     = "rg-ptmmigracion"
-  location = "easus"
+  location = "eastus"
 
     tags = {
         environment = "production"
