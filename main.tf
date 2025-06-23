8c9eac9690ccd050cc06bcb1a900e9da7e4274ed
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-06-22 23:03:56 -0600

    se modifica .yml

--- main.tf
+++ main.tf
@@ -8,15 +8,12 @@ module "resource_groups" {
 
 module "vnets" {
   source = "./modules/vnets"
-
   for_each = { for vnet in var.vnets : vnet.name => vnet }
 
-  vnet_name          = each.value.name
-  address_space      = each.value.address_space
-  location           = each.value.location
+  vnet_name         = each.value.name
+  address_space     = each.value.address_space
+  location          = each.value.location
   resource_group_name = each.value.resource_group
 
-  depends_on = [
-    module.resource_groups[each.value.resource_group]
-  ]
+  depends_on = [module.resource_groups]  # <- esta es la forma válida
 }
