c823aceef482246cc7fac950886986f01ab7ced3
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-06-22 21:41:06 -0600

    v8

--- modules/vnets/main.tf
+++ modules/vnets/main.tf
@@ -1,12 +1,10 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-pruebas"
-  address_space       = ["10.0.0.0/16"]
-  resource_group_name = azurerm_resource_group.rg.name.rg2
-  location           = azurerm.resource_group.location.rg2
-
+    name                = "vnet-pruebas"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.rg2.location
+    resource_group_name = azurerm_resource_group.rg2.name
     tags = {
         environment = "production"
         owner       = "team-a"
     }
-
 }
