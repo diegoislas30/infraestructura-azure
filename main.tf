4e308243115c0fec04ab2e556aba65b45428a835
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-06-22 19:58:15 -0600

    se corrige nombre

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
-
 module "resource_group" {
-  source = "./modules/resource_group"
-
-  resource_group_name = var.resource_group_name
-  location            = var.locatiom
+  source   = "./modules/resources_group"
+  name     = var.rg_name
+  location = var.location
   
 }
-
