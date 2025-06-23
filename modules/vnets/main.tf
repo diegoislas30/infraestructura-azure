6dfc6856e62a7f2b132d3c76991eb70558f4684e
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-06-22 22:56:03 -0600

    a ver si jala 2

--- modules/vnets/main.tf
+++ modules/vnets/main.tf
@@ -3,4 +3,6 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.address_space
   location            = var.location
   resource_group_name = var.resource_group_name
+
+  depends_on = [ azurerm_resource_group.this ]
 }
