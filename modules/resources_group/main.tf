460f59f192efd9249b38d334e4a5c8be79c9e9db
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-06-22 21:29:50 -0600

    se añade rg ptm

--- modules/resources_group/main.tf
+++ modules/resources_group/main.tf
@@ -2,3 +2,16 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-pruebas"
   location = "eastus"
 }
+
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "rg-ptmmigracion"
+  location = "easus"
+
+    tags = {
+        environment = "production"
+        owner       = "team-a"
+    }
+}
+
+
